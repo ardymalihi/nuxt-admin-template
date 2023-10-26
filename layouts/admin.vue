eff730b36e7f449ce06d49f4b71d48eb46acaf70
Ardy Malihi <ardalan.malihi@gmail.com>
2023-10-26 20:20:00 +0300

    using hero icon

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -9,7 +9,7 @@ const sidebar = ref<HTMLDivElement>();
 
 async function logout() {
     await client.auth.signOut();
-    router.push("/");
+    router.push("/login");
 }
 
 
@@ -19,16 +19,23 @@ function toggleSidebar() {
 </script>
 <template>
     <div class="bg-gradient-to-br from-black via-cyan-700 to-pink-200 flex min-h-screen">
-        <div class="flex w-screen m-10 rounded-md overflow-hidden">
+        <div class="flex w-screen rounded-md overflow-hidden">
             <!-- Sidebar Pane -->
             <div v-show="sidebarOpen" ref="sidebar"
-                class="w-[300px] bg-gray-500 m-10 absolute inset-y-0 left-0 rounded-tl-md rounded-bl-md overflow-hidden">
+                class="w-[300px] text-blue-200 bg-gray-900 bg-opacity-95 absolute inset-y-0 left-0 rounded-md overflow-hidden">
                 <div class="flex h-[64px]">
-                    <div class="flex-row bg-green-500 p-5" @click="toggleSidebar">close</div>
+                    <div class="flex-row p-5" @click="toggleSidebar">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 cursor-pointer">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+
+                    </div>
                     <div class="flex-row ml-5 p-5">logo</div>
                 </div>
                 <!-- Menu Items -->
-                <div class="flex bg-yellow-500 w-max p-3 m-3">
+                <div class="flex w-max p-3 m-3">
                     Menu 1
                 </div>
 
@@ -36,8 +43,17 @@ function toggleSidebar() {
             <!-- Main Pane -->
             <div class="w-[100%]" ref="main">
                 <!--Header Pane -->
-                <div class="flex h-[64px] bg-blue-500">
-                    <div class="flex-row bg-green-500 p-5" @click="toggleSidebar">close</div>
+                <div class="flex h-[64px] text-blue-200">
+                    <div class="flex-row p-5" @click="toggleSidebar">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6 cursor-pointer">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+
+
+
+                    </div>
                     <div class="flex-row ml-5 p-5">logo</div>
                     <div class="flex-row w-[100%]"></div>
                     <div v-if="user" class="flex-row  ml-5 p-5">
@@ -49,11 +65,10 @@ function toggleSidebar() {
                     </div>
                 </div>
                 <!-- Content Pane -->
-                <div class="h-[100%] bg-yellow-500 p-5">
+                <div class="h-[100%] p-5">
                     <!-- Content Pane Content -->
                     <slot />
                 </div>
             </div>
         </div>
-    </div>
-</template> 
+</div></template> 
