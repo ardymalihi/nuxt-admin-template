bc6b89041247baca5c476d4a67c8d16830d45b9f
Ardy Malihi <ardalan.malihi@gmail.com>
2023-10-30 14:10:35 +0300

    swich from div to table

--- components/Table.vue
+++ components/Table.vue
@@ -5,6 +5,7 @@ export type IColumnConfig = {
 }
 export interface ITableConfig {
     tableName: string;
+    allowDelete: boolean;
     columns: IColumnConfig[] | null,
 }
 const props = defineProps<{
@@ -17,12 +18,16 @@ console.log(props.config.columns?.map(c => c.fieldName).join(","));
 const { data: rows, error } = await client.from(props.config.tableName).select(props.config.columns?.map(c => c.fieldName).join(","));
 </script>
 <template>
-    <div class="shadow-md bg-cyan-500 rounded-md overflow-hidden">
-        <div class="flex h-12">
-            <div v-for="column in props.config.columns" class="w-1/3 p-2 bg-cyan-500 text-white">{{ column.title }}</div>
-        </div>
-        <div v-for="row in rows" class="flex group bg-white hover:bg-gray-100 h-12">
-            <div v-for="column in props.config.columns" class="w-1/3 p-2 border-b border-gray-300">{{ (row as any)[column.fieldName] }}</div>
-        </div>
-    </div>
+    <table class="table-fixed w-[100%] shadow-md rounded-md overflow-hidden">
+        <thead class="bg-cyan-500 h-12">
+            <tr class="text-left text-white">
+                <th v-for="column in props.config.columns" class="p-2">{{ column.title }}</th>
+            </tr>
+        </thead>
+        <tbody class="bg-white">
+            <tr v-for="row in rows" class="border hover:bg-gray-100 h-12">
+                <td v-for="column in props.config.columns" class="p-2">{{ (row as any)[column.fieldName] }}</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
